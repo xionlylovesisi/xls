f20779471e0c57a0c8614419c0a7b693e04202ad
xionlylovesisi <xionlyloveying@163.com>
2017-07-07 12:03:26 +0800

    first commit

---
+++ ThreadTest/src/com/xls/ExecutorCase.java
@@ -0,0 +1,34 @@
+package com.xls;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ExecutorCase {
+	private static Executor executor = Executors.newFixedThreadPool(10,Executors.privilegedThreadFactory());
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			executor.execute(new Task());
+		}
+	}
+	static class Task implements Runnable{
+
+		@Override
+		public void run() {
+			System.out.println(Thread.currentThread().getName());
+		}
+		
+	}
+	static class MyHandler implements RejectedExecutionHandler{
+
+		@Override
+		public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+			System.out.println(Thread.currentThread().getName()+"阻塞");
+		}
+		
+	}
+}
